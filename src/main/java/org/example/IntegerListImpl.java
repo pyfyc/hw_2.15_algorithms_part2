2596f288da9187613791c335d409149a248c1f70
Alex <alexeym75@gmail.com>
2022-10-31 15:30:36 +0700

    Added homework 2.15

---
+++ src/main/java/org/example/IntegerListImpl.java
@@ -0,0 +1,226 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class IntegerListImpl implements IntegerList {
+    private Integer[] storage;
+    private int size;
+
+    public IntegerListImpl() {
+        storage = new Integer[10];
+    }
+
+    public IntegerListImpl(int initSize) {
+        storage = new Integer[initSize];
+    }
+
+    @Override
+    public Integer add(Integer item) {
+        return add(size, item);
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        validateItem(item);
+        validateIndex(index);
+
+        if (size == storage.length) {
+            storage = grow(size + 1);
+        }
+
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
+        size++;
+
+        return item;
+    }
+
+    @Override
+    public Integer set(int index, Integer item) {
+        validateIndex(index);
+        validateItem(item);
+        storage[index] = item;
+        return item;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        validateItem(item);
+
+        int index = indexOf(item);
+
+        return remove(index);
+    }
+
+    @Override
+    public Integer remove(int index) {
+        validateIndex(index);
+
+        Integer item = storage[index];
+        size--;
+
+        if (!(index == size)) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        }
+
+        return item;
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        Integer[] storageCopy = toArray();
+        sortInsertion(storageCopy);
+        return binarySearch(storageCopy, item);
+    }
+
+    @Override
+    public int indexOf(Integer item) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Integer get(int index) {
+        validateIndex(index);
+        return storage[index];
+    }
+
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    private void validateItem(Integer item) {
+        if (item == null) {
+            throw new NullItemException();
+        }
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new InvalidIndexException();
+        }
+    }
+
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int i = 0; i < size; i++) {
+            sb.append(storage[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        return sb.append(']').toString();
+    }
+
+    private Integer[] grow(int capacity) {
+        return storage = Arrays.copyOf(storage, capacity);
+    }
+
+    private void swapElements(Integer[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public void sortBubble(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public void sortSelection(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+
+    public void sortInsertion(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public Integer[] generateRandomArray(int size) {
+        java.util.Random random = new java.util.Random();
+        Integer[] arr = new Integer[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(size);
+        }
+        return arr;
+    }
+
+    private boolean binarySearch(Integer[] arr, Integer item) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == arr[mid]) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,71 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task3();
+    }
+
+    private static void Task1() {
+        IntegerListImpl integerArrayList = new IntegerListImpl();
+
+        integerArrayList.add(1);
+        integerArrayList.add(2);
+        integerArrayList.add(3);
+        integerArrayList.add(4);
+        integerArrayList.add(5);
+        integerArrayList.add(6);
+        integerArrayList.add(7);
+        integerArrayList.add(8);
+        integerArrayList.add(9);
+        integerArrayList.add(10);
+        integerArrayList.add(0, 0);
+        integerArrayList.set(1, 111);
+
+        System.out.println(integerArrayList);
+        System.out.println("Size: " + integerArrayList.size());
+
+        integerArrayList.clear();
+
+        System.out.println(integerArrayList);
+        System.out.println("Size: " + integerArrayList.size());
+    }
+
+    private static void Task2() {
+        IntegerListImpl integerArrayList = new IntegerListImpl();
+
+        Integer[] arr = integerArrayList.generateRandomArray(100_000);
+        Integer[] arr1 = Arrays.copyOf(arr, arr.length);
+        Integer[] arr2 = Arrays.copyOf(arr, arr.length);
+        Integer[] arr3 = Arrays.copyOf(arr, arr.length);
+
+        long start;
+
+        //for (int i = 0; i < arr1.length; i++) {
+        //    System.out.print(arr1[i] + " ");
+        //}
+        start = System.currentTimeMillis();
+        integerArrayList.sortInsertion(arr1);
+        System.out.println("sortInsertion time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        integerArrayList.sortBubble(arr2);
+        System.out.println("sortBubble time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        integerArrayList.sortSelection(arr3);
+        System.out.println("sortSelection time: " + (System.currentTimeMillis() - start));
+    }
+
+    private static void Task3() {
+        IntegerListImpl integerArrayList = new IntegerListImpl();
+
+        integerArrayList.add(1);
+        integerArrayList.add(2);
+        integerArrayList.add(3);
+
+        System.out.println(integerArrayList.contains(1));
+        System.out.println(integerArrayList.contains(11));
     }
 }
